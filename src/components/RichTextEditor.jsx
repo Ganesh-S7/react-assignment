e925d3f2a2e4dfc14abf11d72722f654cb779494
Ganesh S <ganeshsganeshs20@gmail.com>
2025-02-04 15:47:00 +0530

    created richTextEditor component

--- src/components/RichTextEditor.jsx
+++ src/components/RichTextEditor.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react"
+import {  Button, Box,  } from '@mui/material';
+import { RichTextEditor } from '@mantine/rte';
 
-const RichTextEditor = () => {
+
+const RichTextEditorComponent = () => {
+  const [ content, setContent ] = useState(localStorage.getItem('richText')|| '')
+
+  const handleSave = () =>{
+    localStorage.setItem('richText', content);
+    alert(`content saved sucessfully!`);
+  }
   return (
-    <div>RichTextEditor</div>
-  )
-}
+    <>
+    <Box sx={{padding:2}}>
+      <RichTextEditor 
+        value={content} 
+        onChange={setContent}
+       
+        controls={[
+          ['bold', 'italic', 'underline', 'strike'],
+          ['unorderedList', 'orderedList'],
+          ['link', 'image', 'blockquote'],
+          ['clean'],
+      ]}
+      sx={{ minHeight: '300px', width: '100%' }}
+      ></RichTextEditor>
+      <Button variant="contained"  color='primary' onClick={handleSave} sx={{marginTop:2}}>Save</Button>
+    </Box>
+    </>
+  );
+};
 
-export default RichTextEditor
+export default RichTextEditorComponent;
